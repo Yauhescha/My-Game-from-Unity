05378f7a11580756eb93f5361c1e488e94d9b82f
Yauhescha <evgescha.maslov007@yandex.ru>
2021-01-11 21:24:59 +0300

    Добавлty отображение пройденных секунд к уровню

--- Assets/Scripts/FinishController.cs
+++ Assets/Scripts/FinishController.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishController : MonoBehaviour
 {
+  
+
+    [SerializeField]
+    private int secondHard;
+    [SerializeField]
+    private int secondNormal;
+    [SerializeField]
+    private int secondEasy;
+  
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
---
+++ Assets/Scripts/UI/TimerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    private float timer = 0;
+    private Text textUI;
+
+    public float Timer { get => timer; set => timer = value; }
+
+    private void Awake()
+    {
+        textUI = GetComponent<Text>();
+    }
+    private void FixedUpdate()
+    {
+        Timer += Time.deltaTime;
+        textUI.text = Timer.ToString() ;
+    }
+
+}
